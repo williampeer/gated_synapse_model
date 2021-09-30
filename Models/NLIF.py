cb46902d4ad61a154a49343ebfeb481ccf8beecd
William <williapb@gmail.com>
2021-09-30 20:16:57 +0200

    (WIP) NLIF

--- Models/NLIF.py
+++ Models/NLIF.py
@@ -7,14 +7,13 @@ from Models.TORCH_CUSTOM import static_clamp_for, static_clamp_for_matrix
 
 
 class NLIF(nn.Module):
-    parameter_names = ['w', 'E_L', 'tau_m', 'G', 'f_v', 'delta_theta_s', 'b_s', 'delta_V', 'tau_s']  # 0,2,3,5,8
-    parameter_init_intervals = {'E_L': [-64., -55.], 'tau_m': [3.5, 4.0], 'G': [0.7, 0.8],
-                                'f_v': [0.25, 0.35], 'delta_theta_s': [10., 20.], 'b_s': [0.25, 0.35],
-                                'delta_V': [8., 14.], 'tau_s': [5., 6.]}
+    parameter_names = ['w', 'E_L', 'tau_m', 'G', 'tau_g']  # 0,2,3,5,8
+    parameter_init_intervals = {'E_L': [-64., -55.], 'tau_m': [3.5, 4.0], 'G': [0.7, 0.8], 'tau_g': [5., 6.]}
 
     def __init__(self, parameters, N=12, w_mean=0.4, w_var=0.25, neuron_types=T([1, -1])):
         super(NLIF, self).__init__()
         # self.device = device
+        rand_ws = None
 
         if parameters:
             for key in parameters.keys():
@@ -22,38 +21,21 @@ class NLIF(nn.Module):
                     tau_m = FT(torch.ones((N,)) * parameters[key])
                 elif key == 'E_L':
                     E_L = FT(torch.ones((N,)) * parameters[key])
-                elif key == 'G':
-                    G = FT(torch.ones((N,)) * parameters[key])
-                elif key == 'tau_s':
-                    tau_s = FT(torch.ones((N,)) * parameters[key])
-                elif key == 'w_mean':
-                    w_mean = float(parameters[key])
-                elif key == 'w_var':
-                    w_var = float(parameters[key])
-                elif key == 'delta_theta_s':
-                    delta_theta_s = FT(torch.ones((N,)) * parameters[key])
-                elif key == 'b_s':
-                    b_s = FT(torch.ones((N,)) * parameters[key])
-                elif key == 'f_v':
-                    f_v = FT(torch.ones((N,)) * parameters[key])
-                elif key == 'delta_V':
-                    delta_V = FT(torch.ones((N,)) * parameters[key])
+                elif key == 'tau_g':
+                    tau_g = FT(torch.ones((N,)) * parameters[key])
+                elif key == 'preset_weights':
+                    rand_ws = torch.abs(parameters['preset_weights'])
+                    assert rand_ws.shape[0] == N and rand_ws.shape[1] == N, "shape of weights matrix should be NxN"
 
         __constants__ = ['N', 'norm_R_const', 'self_recurrence_mask']
         self.N = N
-        self.norm_R_const = (delta_theta_s - E_L) * 1.1
+        # self.norm_R_const = (delta_theta_s - E_L) * 1.1
 
         self.v = torch.zeros((self.N,))
-        self.s = torch.zeros_like(self.v)  # syn. conductance
-        self.theta_s = delta_theta_s * torch.ones((self.N,))
+        self.g = torch.zeros_like(self.v)  # syn. conductance
 
         self.self_recurrence_mask = torch.ones((self.N, self.N)) - torch.eye(self.N, self.N)
-        if parameters.__contains__('preset_weights'):
-            # print('DEBUG: Setting w to preset weights: {}'.format(parameters['preset_weights']))
-            # print('Setting w to preset weights.')
-            rand_ws = torch.abs(parameters['preset_weights'])
-            assert rand_ws.shape[0] == N and rand_ws.shape[1] == N, "shape of weights matrix should be NxN"
-        else:
+        if rand_ws is None:
             rand_ws = (w_mean - w_var) + 2 * w_var * torch.rand((self.N, self.N))
         nt = T(neuron_types).float()
         self.neuron_types = torch.transpose((nt * torch.ones((self.N, self.N))), 0, 1)
@@ -61,25 +43,15 @@ class NLIF(nn.Module):
         self.self_recurrence_mask = torch.ones((self.N, self.N)) - torch.eye(self.N, self.N)
 
         self.E_L = nn.Parameter(FT(E_L).clamp(-80., -35.), requires_grad=True)
-        self.b_s = nn.Parameter(FT(b_s).clamp(0.01, 0.99), requires_grad=True)
-        self.G = nn.Parameter(FT(G), requires_grad=True)
         self.tau_m = nn.Parameter(FT(tau_m).clamp(1.5, 8.), requires_grad=True)
-        self.tau_s = nn.Parameter(FT(tau_s).clamp(1., 12,), requires_grad=True)
-        self.delta_theta_s = nn.Parameter(FT(delta_theta_s).clamp(6., 30.), requires_grad=True)
-        self.f_v = nn.Parameter(FT(f_v).clamp(0.01, 0.99), requires_grad=True)  # Sample values: f_v = 0.15; delta_V = 12.
-        self.delta_V = nn.Parameter(FT(delta_V).clamp(1., 35.), requires_grad=True)  ##
+        self.tau_g = nn.Parameter(FT(tau_g).clamp(1., 12,), requires_grad=True)
 
         self.register_backward_clamp_hooks()
 
     def register_backward_clamp_hooks(self):
         self.E_L.register_hook(lambda grad: static_clamp_for(grad, -80., -35., self.E_L))
         self.tau_m.register_hook(lambda grad: static_clamp_for(grad, 1.5, 8., self.tau_m))
-        self.tau_s.register_hook(lambda grad: static_clamp_for(grad, 1., 12., self.tau_s))
-        self.G.register_hook(lambda grad: static_clamp_for(grad, 0.01, 0.99, self.G))
-        self.f_v.register_hook(lambda grad: static_clamp_for(grad, 0.01, 0.99, self.f_v))
-        self.delta_theta_s.register_hook(lambda grad: static_clamp_for(grad, 6., 30., self.delta_theta_s))
-        self.delta_V.register_hook(lambda grad: static_clamp_for(grad, 1., 35., self.delta_V))
-        self.b_s.register_hook(lambda grad: static_clamp_for(grad, 0.01, 0.99, self.b_s))
+        self.tau_g.register_hook(lambda grad: static_clamp_for(grad, 1., 12., self.tau_g))
 
         self.w.register_hook(lambda grad: static_clamp_for_matrix(grad, 0., 1., self.w))
 
@@ -89,12 +61,7 @@ class NLIF(nn.Module):
         params_list.append(self.w.data)
         params_list.append(self.E_L.data)
         params_list.append(self.tau_m.data)
-        params_list.append(self.G.data)
-        params_list.append(self.f_v.data)
-        params_list.append(self.delta_theta_s.data)
-        params_list.append(self.b_s.data)
-        params_list.append(self.delta_V.data)
-        params_list.append(self.tau_s.data)
+        params_list.append(self.tau_g.data)
 
         return params_list
 
@@ -104,22 +71,23 @@ class NLIF(nn.Module):
         self.reset_hidden_state()
 
         self.v = self.E_L.clone().detach() * torch.ones((self.N,))
-        self.s = torch.zeros_like(self.v)  # syn. conductance
+        self.g = torch.zeros_like(self.v)  # syn. conductance
 
     def reset_hidden_state(self):
         self.v = self.v.clone().detach()
-        self.s = self.s.clone().detach()
+        self.g = self.g.clone().detach()
         self.theta_s = self.theta_s.clone().detach()
 
     def name(self):
         return self.__class__.__name__
 
     def forward(self, I_ext):
+        # TODO
         W_syn = self.w * self.neuron_types
-        I_syn = (self.s).matmul(self.self_recurrence_mask * W_syn)
+        I_syn = (self.g).matmul(self.self_recurrence_mask * W_syn)
 
-        dv = (self.G * (self.E_L - self.v) + (I_syn + I_ext) * self.norm_R_const) / self.tau_m
-        v_next = torch.add(self.v, dv)
+        # dv = (self.E_L - self.v) + (I_syn + I_ext) * self.norm_R_const) / self.tau_m
+        # v_next = torch.add(self.v, dv)
 
         gating = (v_next / self.theta_s).clamp(0., 1.)
         dv_max = (self.theta_s - self.E_L)
@@ -138,8 +106,8 @@ class NLIF(nn.Module):
         # return self.s * self.tau_s  # use synaptic current as spike signal
         # return self.s * (self.tau_s + 1) / 2.  # return readout of synaptic current as spike signal
 
-        # differentiable soft threshold
-        soft_spiked = torch.sigmoid(torch.sub(v_next, self.theta_s))
-        return soft_spiked  # return sigmoidal spiked
-        # return gating
+        # differentiable
+        # soft_spiked = torch.sigmoid(torch.sub(v_next, self.theta_s))
+        # return soft_spiked  # return sigmoidal spiked
+        return gating
 
